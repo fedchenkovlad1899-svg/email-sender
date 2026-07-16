28345df0727180c8ef53207629638b483201baec
Vlad <fedchenkovlad1899@gmail.com>
2026-07-16 16:21:40 +0300

    add UserListView

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from api.v1.users.views import RegisterView,LoginView,ProfileView
+from api.v1.users.views import RegisterView,LoginView,ProfileView,UserListView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
+    path("profiles/", UserListView.as_view(), name="profiles"),
+
 ]
--- src/api/v1/users/views.py
+++ src/api/v1/users/views.py
@@ -1,16 +1,35 @@
 from rest_framework import generics,status
 from rest_framework.response import Response
 from api.v1.users.serializers import RegisterSerializer,LoginSerializer,UserSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.generics import ListAPIView
 
+from users.models import User
+
+
+
+#GET  /api/users/profiles/
+class UserListView(ListAPIView):
+    """
+    Вывод списка всех пользователей.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
 
 
 #POST /api/users/register/
 class RegisterView(generics.CreateAPIView):
+    """
+       Регистрация пользователя.
+       """
     serializer_class = RegisterSerializer
 
 #POST /api/users/login/
 class LoginView(generics.GenericAPIView):
+    """
+        Авторизация пользователя по email и паролю.
+        """
     serializer_class = LoginSerializer
 
     def post(self, request):
@@ -22,8 +41,13 @@ class LoginView(generics.GenericAPIView):
 
 #GET  /api/users/profile/
 class ProfileView(generics.RetrieveAPIView):
+    """
+       Информация о профиле пользователя.
+       """
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
         return self.request.user
+
+
