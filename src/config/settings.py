745980d448d5ec72d1ed55a44c3a9fd68981dfe3
Vlad <fedchenkovlad1899@gmail.com>
2026-07-22 23:36:35 +0300

    add test_sending_email_solo

--- src/config/settings.py
+++ src/config/settings.py
@@ -203,3 +203,12 @@ SIMPLE_JWT = {
     "SIGNING_KEY": env("SECRET_KEY"),
     "AUTH_HEADER_TYPES": ("JWT",),
 }
+
+#gmail
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env.int("EMAIL_PORT")
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
